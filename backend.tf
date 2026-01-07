4981084eb5d90600b55fba7eb02b30b3faa56951
Faruk1301 <faruk.cse.pust12@gmail.com>
2026-01-07 22:00:37 +0600

    origin

--- backend.tf
+++ backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "faruk-terraform-lab-001"
     key            = "cisco-router/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
     encrypt        = true
+    # dynamodb_table ওয়ার্নিং সরাতে এবং এরর কমাতে আপাতত এটি বাদ রাখছি
+    # যদি পরে দরকার হয় আমরা 'use_lockfile = true' ব্যবহার করবো
   }
 }
