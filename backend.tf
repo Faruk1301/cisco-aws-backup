2f24422f1af91828686b46fd9a45a66d11c7d5f1
Faruk1301 <faruk.cse.pust12@gmail.com>
2026-01-07 23:06:19 +0600

    modifie

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "faruk-terraform-lab-001"
-    key            = "cisco-router/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    # dynamodb_table ওয়ার্নিং সরাতে এবং এরর কমাতে আপাতত এটি বাদ রাখছি
-    # যদি পরে দরকার হয় আমরা 'use_lockfile = true' ব্যবহার করবো
+    bucket  = "faruk-terraform-lab-001"
+    key     = "cisco-router/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    # dynamodb_table এবং অন্যান্য ওয়ার্নিং সৃষ্টিকারী লাইনগুলো বাদ দেওয়া হয়েছে
   }
 }
